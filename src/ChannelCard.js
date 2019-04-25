0a405f043a6896f9c8c68646bd19cf64fe4c67e4
Daniel Simandl <dsimandl419@gmail.com>
2019-04-24 21:35:00 -0500

    channel cards are rending with display images

--- src/ChannelCard.js
+++ src/ChannelCard.js
@@ -1,11 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import { withStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
 
-export default class Channel extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
+const styles = theme => ({
+  card: {
+    maxWidth: 400
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%' // 16:9
+  },
+  actions: {
+    display: 'flex'
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)'
+  },
+  avatar: {
+    backgroundColor: 'red'
   }
-}
+});
+const ChannelCard = props => {
+  const { name, id, player_video_poster_image_url: image } = props.data;
+  const { classes } = props;
+  console.log(props);
+  console.log(name, id, image);
+  return (
+    <Card className={classes.card}>
+      <CardHeader title={name} />
+      <CardMedia className={classes.media} image={image} title={name} />
+    </Card>
+  );
+};
+
+export default withStyles(styles)(ChannelCard);
--- src/ChannelList.js
+++ src/ChannelList.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { withStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
+import ChannelCard from './ChannelCard';
+import Grid from '@material-ui/core/Grid';
 
-export default class ChannelList extends Component {
+// import classes from '*.module.css';
+
+const styles = theme => ({
+  cardList: {
+    display: 'flex'
+  }
+});
+export class ChannelList extends Component {
   constructor(props) {
     super(props);
     this.state = { liveStreams: [] };
   }
   componentDidMount() {
+    this.fetchLiveStreams();
+  }
+
+  fetchLiveStreams() {
     axios.get(`/api/live-streams`).then(({ data }) => {
       const liveStreams = data;
       this.setState({ liveStreams: liveStreams });
@@ -15,13 +30,23 @@ export default class ChannelList extends Component {
 
   render() {
     const { liveStreams } = this.state;
+    const { classes } = this.props;
     console.log(this.state);
     return (
       <div>
-        <ul>
-          {liveStreams && liveStreams.map(stream => <div key={stream.id}>{stream.name}</div>)}
-        </ul>
+        <Grid container spacing={40}>
+          {liveStreams &&
+            liveStreams.map(stream => {
+              return (
+                <Grid item key={stream.id} xs={12} sm={6} md={4} lg={3}>
+                  <ChannelCard data={stream} />
+                </Grid>
+              );
+            })}
+        </Grid>
       </div>
     );
   }
 }
+
+export default withStyles(styles)(ChannelList);
